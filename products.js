867bdc74febef184892fd85375efc700e54d3056
Riael <riaeldoestwitch@gmail.com>
2026-09-13 00:06:54 +0200

    first commit, need to add better info

---
+++ products.js
@@ -0,0 +1,32 @@
+const productsData = [
+    {
+        id: "under-sink-UV",
+        name: "Under-Sink UV Purifier",
+        category: "purifiers",
+        categoryName: "Filters & Purifiers",
+        price: "P850",
+        image: "assets/uvtap.jpg",
+        shortDesc: "It uses ultraviolet light beneath your tap to sanitize drinking water by destroying bacteria and viruses without chemicals.",
+        fullDesc: "Our UV Tap Water Purifier provides an extra layer of protection from drinking water directly from your tap. It combines filtration with UV purification technology, helping reduce imporuties and microorganisms while improving the taste, smell and overall quality of your water."
+    },
+    {
+        id: "central-descaler",
+        name: "Central Water Descaler",
+        category: "descalers",
+        categoryName: "Central Descalers",
+        price: "P3499",
+        image: "assets/centraldescaler.jpg",
+        shortDesc: "Installed on your main line, this central water descaler protects your entire home's plumbing and appliances by preventing hard water scale build-up.",
+        fullDesc: "Our Central Water Descaler helps reduce the effects of hard water throughout your home or business. Installed on the main water line, it helps minimise scale build-up in pipes, geysers, taps, showers and applicates. It helps protect your plumbing system and improve water efficiency."
+    },
+    {
+        id: "tap-water-filter",
+        name: "Tap Water Filter",
+        category: "purifiers",
+        categoryName: "Filters & Purifiers",
+        price: "P150",
+        image: "assets/tapwater.jpg",
+        shortDesc: "Attaching directly to your faucet, this tap filter removes chlorine, impurities, and bad odors to deliver cleaner, fresher-tasting water on demand.",
+        fullDesc: "Our Tap Water Filter provides a simple and convenient way to improve the quality of everyday tap water. It helps reduce common impurities, chlorine, unpleasant tastes and odours, giving you cleaner, fresher-tasting water directly from your tap."
+    }
+];
